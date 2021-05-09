2b565278a8c981d2ebff28ee378d865d46fca589
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-09 21:22:06 +0300

    ObjcTask1_Exercise_3 solved

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,6 +3,40 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
+    
+    if (self.count == 0)
+    {
+        return nil;
+    }
+    
+    NSInteger leftIndex = 0;
+    NSInteger rightIndex = self.count - 1;
+    
+    if (self.count == 1 || ([self[rightIndex] compare:self[leftIndex]] == NSOrderedDescending))
+    {
+        return self[0];
+    }
+    
+    while (rightIndex >= leftIndex)
+    {
+        NSInteger midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+        
+        if ([self[midIndex] compare:self[midIndex + 1]] == NSOrderedDescending)
+        {
+            return self[midIndex + 1];
+        }
+        
+        if ([self[midIndex - 1] compare:self[midIndex]] == NSOrderedDescending)
+        {
+            return self[midIndex];
+        }
+        
+        if ([self[midIndex] compare:self[leftIndex]] == NSOrderedDescending) {
+            leftIndex = midIndex + 1;
+        } else {
+            rightIndex = midIndex - 1;
+        }
+    }
     return nil;
 }
 
