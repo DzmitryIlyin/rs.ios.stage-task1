74605a78e7548e6950c2a75a45b483e6e6f17d50
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-09 21:22:26 +0300

    ObjcTask1_Exercise_4 solved

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,31 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    
+    if ([number isKindOfClass:[NSNull class]])
+    {
+        return nil;
+    }
+    
+    NSString *stringValue = [number stringValue];
+    
+    if ([stringValue containsString:@"-"])
+    {
+        stringValue = [stringValue stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    }
+    
+    NSInteger stringLength = [stringValue length];
+    NSMutableArray *array = [NSMutableArray array];
+    
+    while (stringLength > 0)
+    {
+        stringLength--;
+        NSRange range = NSMakeRange(stringLength, 1);
+        NSString *character = [stringValue substringWithRange:range];
+        [array addObject:character];
+    }
+    
+    return array;
 }
 
 @end
