1c14e41d090b2d850c1b553e78190fd11589e3a3
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-09 21:22:48 +0300

    ObjcTask1_Exercise_5 solved

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,27 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSArray<NSNumber *> *noDuplicates = [[NSOrderedSet orderedSetWithArray:rankedArray] array];
+    NSMutableArray *result = [NSMutableArray array];
+    
+    NSRange searchRange = NSMakeRange(0, [noDuplicates count]);
+    for (NSNumber *number in playerArray) {
+        NSUInteger findIndex = [noDuplicates indexOfObject:number
+                                             inSortedRange:searchRange
+                                             options:NSBinarySearchingInsertionIndex
+                                             usingComparator:^(id obj1, id obj2)
+                                             {
+                                                 return [obj2 compare:obj1];
+                                             }
+                                ];
+        NSNumber *index = @(findIndex + 1);
+        [result addObject: index];
+    }
+    
+    _playersPlaces = [NSArray arrayWithArray:result];
+    
+    return _playersPlaces;
 }
 
 @end
