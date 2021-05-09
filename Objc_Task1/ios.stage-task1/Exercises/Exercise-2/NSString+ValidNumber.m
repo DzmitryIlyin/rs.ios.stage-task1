e40ad8b2f380c668b79fb6793bc0b7533068f4e2
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-09 21:21:41 +0300

    ObjcTask1_Exercise_2 solved

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,6 +3,13 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
+    if (self.length > 0 && self.length <= 100 && self != nil) {
+        NSCharacterSet *numbers = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+        NSString *tempString = [[self componentsSeparatedByCharactersInSet:numbers] componentsJoinedByString:@""];
+        if (self.length == tempString.length) {
+            return true;
+        }
+    }
     return false;
 }
 
